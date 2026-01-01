8f3e453afaaf82abbe6acef0cbfcbf8347d6f41f
shellakajazzy <141759463+shellakajazzy@users.noreply.github.com>
2025-12-31 18:14:45 -0800

    Fix home-manager module placement

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -4,8 +4,8 @@
   home.stateVersion = "25.11";
 
   imports = [
-    ./sops.nix
-    ./ssh.nix
+    ../modules/home-manager/sops.nix
+    ../modules/home-manager/ssh.nix
   ];
 
   programs.home-manager.enable = true;
--- modules/home-manager/sops.nix
+++ modules/home-manager/sops.nix
@@ -3,11 +3,11 @@
 {
   imports = [ inputs.sops-nix.homeManagerModules.sops ];
 
-  home.file.".config/sops/age/keys.txt".source = ../secrets/sops_key.txt;
+  home.file.".config/sops/age/keys.txt".source = ../../secrets/sops_key.txt;
 
   sops = {
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-    defaultSopsFile = ../secrets/secrets.yaml;
+    defaultSopsFile = ../../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
 
     secrets."key_pairs/sops_key/public" = { };
