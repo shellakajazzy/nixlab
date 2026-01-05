a8c93125c846b5b11e5866aa2d28503c238769cf
shellakajazzy@gmail.com <141759463+shellakajazzy@users.noreply.github.com>
2026-01-04 16:50:04 -0800

    Fix poweredge bot not working when not logged into slumber

--- slumber/deploy.nix
+++ slumber/deploy.nix
@@ -20,7 +20,7 @@
       ssh slumber@$(cat ${config.sops.secrets."tailscale/ipv4s/slumber".path}) "rm -rf ~/poweredge-bot.py ~/.config/systemd/user/poweredge-bot.service; mkdir -pv ~/.config/systemd/user; [ -f ~/.local/bin/uv ] || (curl -LsSf https://astral.sh/uv/install.sh | sh)"
       scp ${config.home.homeDirectory}/nixlab/slumber/poweredge-bot.py slumber@$(cat ${config.sops.secrets."tailscale/ipv4s/slumber".path}):~/poweredge-bot.py
       scp ${config.home.homeDirectory}/nixlab/slumber/poweredge-bot.service slumber@$(cat ${config.sops.secrets."tailscale/ipv4s/slumber".path}):~/.config/systemd/user/poweredge-bot.service
-      ssh slumber@$(cat ${config.sops.secrets."tailscale/ipv4s/slumber".path}) "sed -i 's/MAC_ADDRESS/$MAC_ADDRESS/g' ~/poweredge-bot.py; sed -i 's/BOT_KEY/$BOT_KEY/g' ~/poweredge-bot.py; systemctl --user daemon-reload; systemctl --user enable poweredge-bot.service; (systemctl --user restart poweredge-bot.service || systemctl --user start poweredge-bot.service)"
+      ssh slumber@$(cat ${config.sops.secrets."tailscale/ipv4s/slumber".path}) "sed -i 's/MAC_ADDRESS/$MAC_ADDRESS/g' ~/poweredge-bot.py; sed -i 's/BOT_KEY/$BOT_KEY/g' ~/poweredge-bot.py; systemctl --user daemon-reload; systemctl --user enable poweredge-bot.service; (systemctl --user restart poweredge-bot.service || systemctl --user start poweredge-bot.service); loginctl enable-linger slumber"
     '')
   ];
 }
